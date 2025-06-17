8a575871495eefb6508b2ab62956f31dd19f27f7
fasilahammed <ahamedahamed1883@gmail.com>
2025-06-17 08:59:38 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function sendMail() {
+    var params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+    };
+    const serviceID = "service_rcz600d";
+    const templateID = "template_hdw4zgb";
+
+    emailjs
+        .send(serviceID, templateID, params)
+        .then((res) => {
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("message").value = "";
+            console.log(res);
+            alert("your message send successfully");
+
+        })
+        .catch(err => console.log(err));
+}
+
