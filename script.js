e7559a7084bb0aca62b485318c726d3fb6ac8c6c
fasilahammed <ahamedahamed1883@gmail.com>
2025-06-17 09:29:06 +0530

    first commit

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-function sendMail() {
-    var params = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        message: document.getElementById("message").value,
+function sendEmail( event){
+    event.preventDefault();
+    let params={
+        name:document.getElementById("form-name").value,
+        Email:document.getElementById("InputEmail1").value,
+        message:document.getElementById("FormControlTextarea1").value,
     };
-    const serviceID = "service_rcz600d";
-    const templateID = "template_hdw4zgb";
+    const serviceID="service_6t73qxr";
+    const templateID="template_hdw4zgb";
 
-    emailjs
-        .send(serviceID, templateID, params)
-        .then((res) => {
-            document.getElementById("name").value = "";
-            document.getElementById("email").value = "";
-            document.getElementById("message").value = "";
-            console.log(res);
-            alert("your message send successfully");
 
-        })
-        .catch(err => console.log(err));
+emailjs.send(serviceID,templateID,params)
+.then(
+    res=>{
+        document.getElementById("form-name").value="";
+        document.getElementById("InputEmail1").value="";
+        document.getElementById("FormControlTextarea1").value="";
+        console.log(res);
+        alert("your message sent succesfully")
+        
+    }
+)
+.catch((err) => console.log(err));
 }
-
